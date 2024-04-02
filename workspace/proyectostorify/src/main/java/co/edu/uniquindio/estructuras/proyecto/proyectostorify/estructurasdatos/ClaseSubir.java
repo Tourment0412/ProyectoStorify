6b3e6c2260e59f7d588ebd57e2ef31c2a06fd089
JUANMANUELUQ <juanm.isazav@uqvirtual.edu.co>
2024-04-02 17:55:13 -0500

    Estructura de colas ingresadas
    Se ingresó la estructura personalizada de datos cola junto con su respectivo nodo

--- workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/estructurasdatos/ClaseSubir.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.estructuras.proyecto.proyectostorify.estructurasdatos;
-
-public class ClaseSubir {
-
-}
---
+++ workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/estructurasdatos/Nodo.java
@@ -0,0 +1,52 @@
+package co.edu.uniquindio.estructuras.proyecto.proyectostorify.estructurasdatos;
+
+public class Nodo<E> {
+
+	Nodo<E> nextNode;
+	E value;
+
+	/**
+	 * Metodo constructor
+	 * @param valorNodo Valor del nodo
+	 */
+	public Nodo(E valorNodo) {
+		this.value = valorNodo;
+		nextNode = null;
+	}
+
+	/**
+	 * Obtiene el siguiente nodo
+	 * @return Siguiente nodo
+	 */
+	public Nodo getNextNode() {
+		return nextNode;
+	}
+
+	
+	/**
+	 * Cambia el siguiente nodo
+	 * @param siguienteNodo Nuevo siguiente nodo
+	 */
+	public void setNextNode(Nodo siguienteNodo) {
+		this.nextNode = siguienteNodo;
+	}
+
+	/**
+	 * Obtiene el valor del nodo
+	 * @return Valor del nodo
+	 */
+	public E getValue() {
+		return value;
+	}
+
+	/**
+	 * Cambia el valor del nodo
+	 * @param valorNodo Nuevo valor del nodo
+	 */
+	public void setValue(E valorNodo) {
+		this.value = valorNodo;
+	}
+	
+	
+
+}
