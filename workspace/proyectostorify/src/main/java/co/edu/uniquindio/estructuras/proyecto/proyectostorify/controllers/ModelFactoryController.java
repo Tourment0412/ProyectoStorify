b4935208ebd4bac230b3ffafa2cb142bbb9bbdf6
JUANMANUELUQ <juanm.isazav@uqvirtual.edu.co>
2024-04-09 22:22:59 -0500

    Some new things in the modelFactory

--- workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/controllers/ModelFactoryController.java
+++ workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/controllers/ModelFactoryController.java
@@ -3,6 +3,9 @@ package co.edu.uniquindio.estructuras.proyecto.proyectostorify.controllers;
 import java.util.List;
 
 import co.edu.uniquindio.estructuras.proyecto.proyectostorify.application.App;
+import co.edu.uniquindio.estructuras.proyecto.proyectostorify.circularList.CircularList;
+import co.edu.uniquindio.estructuras.proyecto.proyectostorify.model.*;
+import co.edu.uniquindio.estructuras.proyecto.proyectostorify.stack.Stack;
 import javafx.stage.Stage;
 
 public class ModelFactoryController {
@@ -10,6 +13,9 @@ public class ModelFactoryController {
 
 	private App aplicacion;
 	private Stage ventana;
+	Storify tiendaMusica;
+	Stack<Cancion> pilaCanciones;
+	Usuario usuarioSesion;
 	
 	private static class SingletonHolder { 
 		// El constructor de Singleton puede ser llamado desde aqu� al ser protected
@@ -42,6 +48,62 @@ public class ModelFactoryController {
 
 	public void setVentana(Stage ventana) {
 		this.ventana = ventana;
+	}
+	
+	public Cuenta obtenerCuenta(String nombre) {
+		return tiendaMusica.obtenerCuenta(null);
+	}
+	
+	public Cancion obtenerCancion(String codigo) {
+		return tiendaMusica.obtenerCancion(codigo);
+	}
+	
+	public Artista obtenerArtista(String nombre) {
+		return tiendaMusica.obtenerArtista(nombre);
+	}
+	
+	public void agregarUsuario(Usuario usuario) {
+		tiendaMusica.agregarUsuario(usuario);
+	}
+	
+	public void eliminarUsuario(String nombreUsuario) {
+		tiendaMusica.eliminarUsuario(nombreUsuario);
+	}
+	
+	public void agregarCancion(Cancion cancion,Artista artista) {
+		tiendaMusica.agregarCancion(cancion, artista);
+	}
+	
+	public void eliminarCancion(Cancion cancion) {
+		tiendaMusica.eliminarCancion(cancion);
+	}
+	
+	public void agregarArtista(Artista artista) {
+		tiendaMusica.agregarArtista(artista);
+	}
+	
+	public void eliminarArtista(Artista artista) {
+		tiendaMusica.eliminarArtista(artista);
+	}
+	
+	public CircularList<Cancion> obtenerCancionesFiltradasUnion(String lista,Usuario usuario,String[] datos) {
+		return tiendaMusica.obtenerCancionesFiltradasUnion(lista, usuario, datos);
+	}
+	
+	public CircularList<Cancion> obtenerCancionesFiltradasInterseccion(String lista,Usuario usuario,String[] datos) {
+		return tiendaMusica.obtenerCancionesFiltradasInterseccion(lista, usuario, datos);
+	}
+	
+	public void agregarCambioCancion(Cancion cancion,String accion) {
+		pilaCanciones.push(cancion, accion);
+	}
+	
+	public void deshacerCambioCancion() {
+		String accion=pilaCanciones.headAction();
+		Cancion cancion=pilaCanciones.pop();
+		switch(accion) {
+		
+		}
 	}
 	
 }
