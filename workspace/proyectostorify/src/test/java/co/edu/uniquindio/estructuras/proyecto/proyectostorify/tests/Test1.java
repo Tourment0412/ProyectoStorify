aaa33e90df0c2e44debee30598ec6a0fa493ec3e
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-05-02 20:26:14 -0500

    Added dependency for JUnit Test Cases

---
+++ workspace/proyectostorify/src/test/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/tests/Test1.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.estructuras.proyecto.proyectostorify.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+		assertEquals(0, 0);
+	}
+
+}
