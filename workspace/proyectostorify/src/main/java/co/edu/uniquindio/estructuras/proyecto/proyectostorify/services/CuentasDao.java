4cce8078b77c705d6e50399c5bca98bb99aa18a8
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-05-10 15:30:17 -0500

    Finished Dao classes

--- workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/services/CuentasDao.java
+++ workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/services/CuentasDao.java
@@ -11,39 +11,61 @@ import co.edu.uniquindio.estructuras.proyecto.proyectostorify.model.Cuenta;
 import lombok.Getter;
 
 public class CuentasDao {
+	/**
+	 * variable for singleton of the class
+	 */
 	private static CuentasDao instance;
+
+	/**
+	 * Constant indicating the path of the persistence file
+	 */
 	@Getter
-	private static final String RUTA= "src/main/resources/co/edu/uniquindio/estructuras/proyecto/proyectostorify/data/cuentas.dat";
-	
-	
+	private static final String RUTA = "src/main/resources/co/edu/uniquindio/estructuras/proyecto/proyectostorify/data/cuentas.dat";
+
+	/**
+	 * Method to return the instance of the class in memory, if it does not exist.
+	 * 
+	 * @return instance
+	 */
 	public static CuentasDao getInstance() {
-		if(instance==null) {
-			instance= new CuentasDao();
+		if (instance == null) {
+			instance = new CuentasDao();
 		}
 		return instance;
 	}
-	
+
+	/**
+	 * Method for saving the list {@link HashMap} of accounts (users) in the
+	 * serialization file
+	 * 
+	 * @param lstCuentas
+	 */
 	public void saveData(HashMap<String, Cuenta> lstCuentas) {
-		try(ObjectOutputStream oos= new ObjectOutputStream(new FileOutputStream(getRUTA()))) {
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(getRUTA()))) {
 			oos.writeObject(lstCuentas);
 			oos.close();
-		}catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Method for loading the list (map) of accounts from the serialization file
+	 * 
+	 * @return {@link HashMap}
+	 */
 	@SuppressWarnings("unchecked")
-	public HashMap<String, Cuenta> loadData(){
-		try(ObjectInputStream ois= new ObjectInputStream(new FileInputStream(getRUTA()))){
-			Object objeto= ois.readObject();
+	public HashMap<String, Cuenta> loadData() {
+		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(getRUTA()))) {
+			Object objeto = ois.readObject();
 			ois.close();
 			return (HashMap<String, Cuenta>) objeto;
-			
-		}catch(IOException | ClassNotFoundException e) {
-			HashMap<String, Cuenta> hashmap= new HashMap<String, Cuenta>();
+
+		} catch (IOException | ClassNotFoundException e) {
+			HashMap<String, Cuenta> hashmap = new HashMap<String, Cuenta>();
 			saveData(hashmap);
-			e.printStackTrace();
+			return hashmap;
 		}
-		return null;
+
 	}
 }
