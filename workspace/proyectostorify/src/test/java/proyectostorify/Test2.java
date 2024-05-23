e8ff6a36ede35008ca735b5997a0409fc40c4b34
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-05-22 22:54:42 -0500

    Now able to create JUnits on text\java

---
+++ workspace/proyectostorify/src/test/java/proyectostorify/Test2.java
@@ -0,0 +1,19 @@
+package proyectostorify;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test2 {
+
+	@Test
+	public void testSetUrl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGenero() {
+		fail("Not yet implemented");
+	}
+
+}
