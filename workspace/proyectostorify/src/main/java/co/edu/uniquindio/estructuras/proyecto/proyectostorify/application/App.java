bd8bdccd59d1705a239d9a660f89dcebf73a4248
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2024-05-28 15:54:22 -0500

    ChangesAlertPopularity

--- workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/application/App.java
+++ workspace/proyectostorify/src/main/java/co/edu/uniquindio/estructuras/proyecto/proyectostorify/application/App.java
@@ -2,7 +2,6 @@ package co.edu.uniquindio.estructuras.proyecto.proyectostorify.application;
 
 import javafx.application.Application;
 
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -18,80 +17,81 @@ import java.io.IOException;
 import co.edu.uniquindio.estructuras.proyecto.proyectostorify.circularList.CircularList;
 import co.edu.uniquindio.estructuras.proyecto.proyectostorify.controllers.*;
 
-
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-	
 	private Stage primaryStage;
 	private AnchorPane rootLayout;
-	
+
 	/**
 	 * Metodo que inica la ejecucion de la aplicacion
 	 */
 	@Override
-	public void start(Stage primaryStage) throws IOException{
+	public void start(Stage primaryStage) throws IOException {
 		// Cargar la imagen del icono desde el classpath
-        String iconPath = "/co/edu/uniquindio/estructuras/proyecto/proyectostorify/images/logo2.png";
-        Image icon = new Image(getClass().getResourceAsStream(iconPath));
-        
-     // Establecer la imagen como el icono del Stage principal
-        primaryStage.getIcons().add(icon);
-		
+		String iconPath = "/co/edu/uniquindio/estructuras/proyecto/proyectostorify/images/logo2.png";
+		Image icon = new Image(getClass().getResourceAsStream(iconPath));
+
+		// Establecer la imagen como el icono del Stage principal
+		primaryStage.getIcons().add(icon);
+
 		ModelFactoryController mfm = ModelFactoryController.getInstance();
 		mfm.setAplicacion(this);
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Storify");
 		mostrarIniciarSesion();
 	}
-	
+
 	/**
 	 * Muestra la ventana del administrador
 	 */
 	public void mostrarAdministrador() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Administrador.fxml"));
+			loader.setLocation(App.class
+					.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Administrador.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.centerOnScreen();
 			primaryStage.show();
 			AdministradorController controlador = loader.getController();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Muestra la ventana del usuario
 	 */
 	public void mostrarUsuario() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Usuario.fxml"));
+			loader.setLocation(
+					App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Usuario.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.centerOnScreen();
 			primaryStage.show();
 			UsuarioController controlador = loader.getController();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Inicia la ventana principal
 	 */
 	public void mostrarIniciarSesion() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/IniciarSesion.fxml"));
+			loader.setLocation(App.class
+					.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/IniciarSesion.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
@@ -99,19 +99,20 @@ public class App extends Application {
 			primaryStage.show();
 			IniciarSesionController controlador = loader.getController();
 			controlador.setVentana(primaryStage);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Muestra la ventana de administrador de artistas
 	 */
 	public void mostrarAdministradorArtistas() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/AdministradorArtistas.fxml"));
+			loader.setLocation(App.class.getResource(
+					"/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/AdministradorArtistas.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
@@ -119,20 +120,20 @@ public class App extends Application {
 			primaryStage.show();
 			AdministradorArtistasController controlador = loader.getController();
 			controlador.setVentana(primaryStage);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/**
 	 * Muestra la ventana de administracion de canciones
 	 */
 	public void mostrarAdministradorCanciones() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/AdministradorCanciones.fxml"));
+			loader.setLocation(App.class.getResource(
+					"/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/AdministradorCanciones.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
@@ -140,20 +141,20 @@ public class App extends Application {
 			primaryStage.show();
 			AdministradorCancionesController controlador = loader.getController();
 			controlador.setVentana(primaryStage);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/**
 	 * Muestra la ventana de los artistas
 	 */
 	public void mostrarArtistas() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Artistas.fxml"));
+			loader.setLocation(App.class
+					.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Artistas.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
@@ -161,20 +162,20 @@ public class App extends Application {
 			primaryStage.show();
 			ArtistasController controlador = loader.getController();
 			controlador.setVentana(primaryStage);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/**
 	 * Muestra la ventana de lista de canciones
 	 */
 	public void mostrarListaCanciones() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/ListaCanciones.fxml"));
+			loader.setLocation(App.class
+					.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/ListaCanciones.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
@@ -182,20 +183,20 @@ public class App extends Application {
 			primaryStage.show();
 			ListaCancionesController controlador = loader.getController();
 			controlador.setVentana(primaryStage);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/**
 	 * Muestra la ventana para el registro de usuarios
 	 */
 	public void mostrarResgistrarse() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Registrarse.fxml"));
+			loader.setLocation(App.class
+					.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/Registrarse.fxml"));
 			rootLayout = (AnchorPane) loader.load();
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
@@ -203,22 +204,27 @@ public class App extends Application {
 			primaryStage.show();
 			RegistrarseController controlador = loader.getController();
 			controlador.setVentana(primaryStage);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Muestra la ventana para reproducir audios
+	 * 
 	 * @param archivosReproducir Archivos de audio a reproducir
 	 */
 	public void reproducirCancion(CircularList<File> archivosReproducir) {
 		try {
+			String iconPath = "/co/edu/uniquindio/estructuras/proyecto/proyectostorify/images/logo2.png";
+			Image icon = new Image(getClass().getResourceAsStream(iconPath));
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(App.class.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/videoplayer.fxml"));
+			loader.setLocation(App.class
+					.getResource("/co/edu/uniquindio/estructuras/proyecto/proyectostorify/fxml/videoplayer.fxml"));
 			BorderPane page = (BorderPane) loader.load();
 			Stage dialogStage = new Stage();
+			dialogStage.getIcons().add(icon);
 			dialogStage.setTitle("Reproductor");
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.initOwner(primaryStage);
@@ -227,8 +233,8 @@ public class App extends Application {
 			VideoPlayerController controller = loader.getController();
 			controller.organizarArchivos(archivosReproducir);
 			dialogStage.setOnCloseRequest(event -> {
-				 controller.stop();
-		        });
+				controller.stop();
+			});
 			dialogStage.showAndWait();
 		} catch (IOException e) {
 			e.printStackTrace();
--- workspace/proyectostorify/src/test/java/proyectostorify/Test2.java
+++ workspace/proyectostorify/src/test/java/proyectostorify/Test2.java
@@ -10,25 +10,31 @@ import co.edu.uniquindio.estructuras.proyecto.proyectostorify.doubleList.ListaDo
 
 public class Test2 {
 
+	/**
+	 * Prueba el iterador de CircularList.
+	 */
 	@Test
 	public void pruebaIteradorCircularList() {
-		CircularList<String> lista=new CircularList<String>();
+		CircularList<String> lista = new CircularList<String>();
 		lista.add("a");
 		lista.add("b");
 		lista.add("c");
 		lista.add("d");
 		lista.add("e");
 		lista.add("f");
-		String msj="";
+		String msj = "";
 		for (String elemento : lista) {
-			msj+=elemento;
+			msj += elemento;
 		}
-		assertEquals(msj,"abcdef");
+		assertEquals(msj, "abcdef");
 	}
 
+	/**
+	 * Prueba el método de eliminación de CircularList.
+	 */
 	@Test
 	public void pruebaEliminarCircularList() {
-		CircularList<Integer> lista=new CircularList<Integer>();
+		CircularList<Integer> lista = new CircularList<Integer>();
 		lista.add(1);
 		lista.add(2);
 		lista.add(3);
@@ -39,15 +45,18 @@ public class Test2 {
 		lista.add(8);
 		lista.add(9);
 		lista.add(10);
-		lista.remove((Integer)6);
-		lista.remove((Integer)1);
-		lista.remove((Integer)6);
-		assertEquals(lista.size(),8);
+		lista.remove((Integer) 6);
+		lista.remove((Integer) 1);
+		lista.remove((Integer) 6);
+		assertEquals(lista.size(), 8);
 	}
-	
+
+	/**
+	 * Prueba BinaryTree.
+	 */
 	@Test
 	public void pruebaArbol() {
-		BinaryTree<Integer> arbol=new BinaryTree<Integer>();
+		BinaryTree<Integer> arbol = new BinaryTree<Integer>();
 		arbol.add(10);
 		arbol.add(20);
 		arbol.add(30);
@@ -58,12 +67,15 @@ public class Test2 {
 		arbol.remove(20);
 		arbol.remove(17);
 		arbol.remove(54);
-		assertEquals(arbol.toCircularList().size(),4);
+		assertEquals(arbol.toCircularList().size(), 4);
 	}
-	
+
+	/**
+	 * Prueba BinaryTree con recorrido en postorden.
+	 */
 	@Test
 	public void pruebaArbolCadenaPostOrden() {
-		BinaryTree<Integer> arbol=new BinaryTree<Integer>();
+		BinaryTree<Integer> arbol = new BinaryTree<Integer>();
 		arbol.add(10);
 		arbol.add(20);
 		arbol.add(30);
@@ -74,43 +86,52 @@ public class Test2 {
 		arbol.remove(20);
 		arbol.remove(17);
 		arbol.remove(54);
-		assertEquals(arbol.toCircularList().toString(),"[34,30,12,10]");
+		assertEquals(arbol.toCircularList().toString(), "[34,30,12,10]");
 	}
-	
+
+	/**
+	 * Prueba ListaDoble.
+	 */
 	@Test
 	public void pruebaListaDoble() {
-		ListaDoble<Double> lista=new ListaDoble<Double>();
+		ListaDoble<Double> lista = new ListaDoble<Double>();
 		lista.agregar(2.0);
 		lista.agregar(5.0);
 		lista.agregar(2.4);
 		lista.agregar(8.2);
 		lista.eliminar(2.0);
-		assertEquals(lista.getTamanio(),3);
+		assertEquals(lista.getTamanio(), 3);
 	}
-	
+
+	/**
+	 * Prueba ListaDoble con representación en cadena.
+	 */
 	@Test
 	public void pruebaListaDobleString() {
-		ListaDoble<Double> lista=new ListaDoble<Double>();
+		ListaDoble<Double> lista = new ListaDoble<Double>();
 		lista.agregar(2.0);
 		lista.agregar(5.0);
 		lista.agregar(2.4);
 		lista.agregar(8.2);
 		lista.eliminar(2.0);
-		assertEquals(lista.toString(),"[5.0,2.4,8.2]");
+		assertEquals(lista.toString(), "[5.0,2.4,8.2]");
 	}
-	
+
+	/**
+	 * Prueba la unión de dos CircularList.
+	 */
 	@Test
 	public void pruebaUnionCircularList() {
-		ListaDoble<Double> lista=new ListaDoble<Double>();
+		ListaDoble<Double> lista = new ListaDoble<Double>();
 		lista.agregar(2.0);
 		lista.agregar(5.0);
 		lista.agregar(2.4);
 		lista.agregar(8.2);
-		CircularList<Double> listaCircular=new CircularList<Double>();
+		CircularList<Double> listaCircular = new CircularList<Double>();
 		listaCircular.add(5.0);
 		listaCircular.add(4.5);
 		listaCircular.setUnion(lista.toCircularList());
-		assertEquals(listaCircular.toString(),"[5.0,4.5,2.0,2.4,8.2]");
+		assertEquals(listaCircular.toString(), "[5.0,4.5,2.0,2.4,8.2]");
 	}
-	
+
 }
